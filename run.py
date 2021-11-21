ca7dab714da74425d7073ffa629e58172bdf1381
kreilly86 <kirstyrebeccareilly@gmail.com>
2021-11-21 19:13:18 +0000

    re-order questions to fit terminal character limit

--- run.py
+++ run.py
@@ -16,26 +16,43 @@ SHEET = GSPREAD_CLIENT.open('personality_profiler')
 print('----Welcome to the Personality Profiler----')
 print("  -----Let's explore your traits!-----\n")
 
-class Question: 
+
+class Question:
+    """
+    Creates class instance
+    of survey question
+    """
     def __init__(self, prompt, answer):
         self.prompt = prompt
         self.answer = answer
 
 
 question_prompts = [
-    "You are more likely to feel energised by?\n(a) getting some alone time\n(b) going out with some friends\n\n",
-    "You feel more like yourself when you're...\n(a) the center of attention\n(b) in the background\n\n",
-    "You find talking to a stranger:\n(a) energising!\n(b) uncomfortable\n\n",
-    "You would hate working with someone who is:\n(a) timid and meek\n(b) loud and overbearing\n\n",
-    "Generally, which of these do you normally feel?\n(a) overwhelmed and overstimulated\n(b) bored and understimulated\n\n",
-    "You usually get more joy out of:\n(a) reading a great book\n(b) watching a great movie\n\n",
-    "The people who know you best would describe you as someone who is...\n(a) outgoing and talkative\n(b) quiet and reflective\n\n",
-    "In your free time on a weekend, would you prefer?\n(a) a deep conversation with a close friend\n(b) mingling at a party with people you've never met before\n\n",
-    "You are more productive when you're in a:\n(a) cafe\n(b) quiet room\n\n",
-    "When you meet someone for the first time:\n(a) you usually do most of the listening\n(b) you usually do most of the talking\n",
-] 
+    """You are more likely to feel energised by?\n(a) getting some alone time
+    (b) going out with some friends\n""",
+    """You feel more like yourself when you're\n(a) the center of attention
+    (b) in the background\n""",
+    """You find talking to a stranger:\n(a) energising!
+    (b) uncomfortable\n""",
+    """You would hate working with someone who is\n(a) timid and meek
+    (b) loud and overbearing\n""",
+    """Generally, which of these do you normally feel?\n(a) overwhelmed and
+    overstimulated\n(b) bored and understimulated\n""",
+    """You usually get more joy out of:\n(a) reading a great book
+    (b) watching a great movie\n""",
+    """The people who know you best would describe you as someone who is
+    (a) outgoing and talkative\n(b) quiet and reflective\n""",
+    """In your free time on a weekend, would you prefer?
+    (a) a deep conversation with a close friend
+    (b) mingling at a party with people you've never met before\n""",
+    """You are more productive when you're in a:\n(a) cafe
+    (b) quiet room\n""",
+    """When you meet someone for the first time:
+    (a) you usually do most of the listening
+    (b) you usually do most of the talking\n""",
+]
 
-# question with answers equating to extraversion quality
+# questions with answer equating to extraversion quality
 
 questions = [
     Question(question_prompts[0], "b"),
@@ -50,10 +67,10 @@ questions = [
     Question(question_prompts[9], "b"),
 ]
 
+
 def run_survey(questions):
     """
-    Generate questions for user
-    with input fields giving
+    Generate 10 questions for user returning
     a score out of 10 for extroversion
     """
     score = 0
@@ -61,13 +78,7 @@ def run_survey(questions):
         answer = input(question.prompt)
         if answer == question.answer:
             score += 1
-    print(str(score) + "/" + str(len(questions)) +  "extrovert")
+    print(str(score) + "/" + str(len(questions)) + "extrovert")
+
 
 run_survey(questions)
-
-# work out how to return score and then calculate introvert, extrovert or ambivert
-# if score > 70% = extrovert
-# if score < 40% = introvet
-# if score 50% - 70% = ambivert
-
-
