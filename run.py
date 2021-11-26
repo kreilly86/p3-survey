1b7e1722ee6f707baa9611ec289bf6bf14cd1b8a
kreilly86 <kirstyrebeccareilly@gmail.com>
2021-11-26 15:32:01 +0000

    Create functions to capture user details and function to ask further questions

--- run.py
+++ run.py
@@ -14,7 +14,22 @@ SHEET = GSPREAD_CLIENT.open('personality_profiler')
 
 print('-------------------------------------------')
 print('----Welcome to the Personality Profiler----')
-print("  -----Let's explore your traits!-----\n")
+print("  -----Let's explore your trait!-----\n\n")
+print('First we would like to ask for some details\n')
+
+
+def capture_user():
+    """
+    Get user info such as
+    name, age etc
+    """
+    name = input("Please enter your name:\n\n")
+    print("Hello", name + "!\n")
+    age = input("Please enter your age:\n\n")
+    gender = input("Please enter your gender. Male, female or non-binary\n\n")
+    location = input("Please enter the city you live in\n\n")
+    print("""\nThank you! Now let's dive in and find out more about
+    your personality""")
 
 
 class Question:
@@ -117,12 +132,72 @@ def calculate_trait(score, result):
     or ambivert
     """
     if score >= 7:
-        print(f'\nYou scored {result} you are an extrovert!')
+        print(f'\nYou scored {result} you are an extrovert!\n\n')
     elif score <= 4:
-        print(f'\nYou scored {result} you are an introvert!')
+        print(f'\nYou scored {result} you are an introvert!\n\n')
     else:
-        print(f'\nYou scored {result} you are an ambivert!')
+        print(f'\nYou scored {result} you are an ambivert!\n\n')
 
 
+pandemic_prompts = [
+    """Question 1.What was your reaction to the dramatic change in
+    social contact the pandemic brought about?\n
+    (a) I was relieved! It gave me time and space to focus
+    on myself and not have pressure to socialise all the time
+    (b) It was extremely hard. I missed socialising and
+    felt something major was missing from my life\n""",
+    """Question 2.Did you notice any change in how you relate
+    to other people. e.g. if you are introverted did you become
+    more extroverted i.e. doing more zoom calls, if you are extroverted
+    did you become more introverted i.e. you became less outward
+    and focused on your interal world a little more\n
+    (a) No,I feel like nothing changed in me and how I naturally relate to
+     others
+    (b) Yes,I feel like I changed and my personality type became less set\n""",
+    """Question 3.During the lockdowns do you feel you learned new skills
+    or took time to do a hobby you always wanted to?
+    (a) Yes! I learned a new skill or took up a hobby I always wanted to
+    (b) No. I wish I used the time to learn new skills or take up a hobby\n""",
+    """Question 4. Did you, like many people decide to buy/adopt a pet during the
+    stay at home period of the pandemic?
+    (a) No! I either had a pet already or didn't get one during the pandemic
+    (b) Yes! I adopted/bought a pet during lockdown. \n""",
+    """Question 5.Do you feel like you really missed activities we used to take
+    for granted e.g. going to a restaurant or meeting friends for drinks?
+    (a) Yes! I really missed activities that were disallowed in the pandemic
+    (b) No, not really I managed just fine!\n""",
+]
+
+pandemic_questions = [
+    Question(pandemic_prompts[0], "a"),
+    Question(pandemic_prompts[1], "b"),
+    Question(pandemic_prompts[2], "a"),
+    Question(pandemic_prompts[3], "b"),
+    Question(pandemic_prompts[4], "a"),
+]
+
+
+def further_questions(pandemic_questions):
+    """
+    Ask a set of 5 questions
+    to find out users' opinion
+    on how the pandemic has effected
+    how they see their personality
+    type, and if they notice any changes
+    """
+    print('Now we have discovered your personality type')
+    print(',we would like to ask a few questions')
+    print('relating to your experience of the pandemic\n')
+
+    for question in pandemic_questions:
+        global answer
+        answer = input(question.prompt)
+        validate_input(answer)
+        if answer == question.answer:
+            print('You answered yes\n')
+
+
+capture_user()
 run_survey(questions)
 calculate_trait(score, result)
+further_questions(pandemic_questions)
